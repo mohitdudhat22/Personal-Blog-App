91e769d2970cd57905f76c5f3c96b414d4e9fd62
mohitdudhatverselix <mohitdudhat22@gmail.com>
2024-09-22 07:07:56 +0530

    changes

--- backend/api/index.js
+++ backend/api/index.js
@@ -32,6 +32,13 @@ const corsOptions = {
   exposedHeaders: ['Content-Range', 'Set-Cookie'],
 };
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
